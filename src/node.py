d5df853ca41014d58b7ed39a061718655977bf2e
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-06-30 10:28:11 -0400

    Update node.py

--- src/node.py
+++ src/node.py
@@ -1,1 +1,15 @@
+import socket
 
+HOST = "127.0.0.1"
+PORT = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM):
+s.connect((HOST, PORT))
+
+message = "hello from client"
+s.sendall(message.encode())
+
+data = s.recv(1024)
+print("Received", data.decode())
+
+s.close()

b3afa83c228cb3f8ce7c69f74c335ee22e181709
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-07-02 17:43:54 -0400

    Change port to 5000 and adjust socket handling
    Updated port number and modified socket connection logic.

--- src/node.py
+++ src/node.py
@@ -1,19 +1,11 @@
 import socket
 
 HOST = "127.0.0.1"
-PORT = 5001
+PORT = 5000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen()
+s.connect((HOST, PORT))
 
-print("waiting for connection...")
+s.sendall("hello from node".encode())
 
-conn, addr = s.accept()
-print("connected:", addr)
-
-data = conn.recv(1024)
-print("got:", data.decode())
-
-conn.close()
 s.close()

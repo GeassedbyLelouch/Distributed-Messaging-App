22f87ee1a759da26e41d1a30c916a9ad51c2e0e4
Abraham_Lincoln <159060686+GeassedbyLelouch@users.noreply.github.com>
2026-07-02 19:03:33 -0400

    Modify message sending to use user input
    Replace static message with user input and protocol encoding.

--- src/node.py
+++ src/node.py
@@ -1,4 +1,5 @@
 import socket
+import protocol
 
 HOST = "127.0.0.1"
 PORT = 5000
@@ -6,6 +7,12 @@ PORT = 5000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-s.sendall("hello from node".encode())
+line = input("Hello! What would you like to send today?: ")
+
+final_line = protocol.encode({
+  "Hello from node": line    
+})
+
+s.sendall(final_line)
 
 s.close()

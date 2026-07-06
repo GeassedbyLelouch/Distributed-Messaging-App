4b5264824eb03ea10ee140af2caf383925916c71
supasuge <epardo1742@proton.me>
2026-07-06 17:14:03 -0400

    test(attestation): cover reverse split, nonce exhaustion, 2-output hkdf

--- tests/test_attestation_noise_state.py
+++ tests/test_attestation_noise_state.py
@@ -42,3 +42,26 @@ def test_split_gives_directional_channels():
     chan_b = noise.SecureChannel(b_second, b_first, b.handshake_hash)
     # initiator's send channel pairs with responder's recv channel
     assert chan_b.recv.decrypt_with_ad(b"", chan_a.send.encrypt_with_ad(b"", b"m")) == b"m"
+
+
+def test_split_reverse_direction_also_pairs():
+    a = noise._SymmetricState(b"Noise_TEST"); a.mix_key(b"\x11" * 32)
+    b = noise._SymmetricState(b"Noise_TEST"); b.mix_key(b"\x11" * 32)
+    a_send, a_recv = a.split()
+    b_first, b_second = b.split()
+    chan_a = noise.SecureChannel(a_send, a_recv, a.handshake_hash)
+    chan_b = noise.SecureChannel(b_second, b_first, b.handshake_hash)
+    # responder's send channel pairs with initiator's recv channel
+    assert chan_a.recv.decrypt_with_ad(b"", chan_b.send.encrypt_with_ad(b"", b"r")) == b"r"
+
+
+def test_cipherstate_nonce_exhaustion_raises():
+    cs = noise._CipherState(b"\x00" * 32)
+    cs._n = noise.MAX_NONCE + 1
+    with pytest.raises(HandshakeError):
+        cs.encrypt_with_ad(b"", b"x")
+
+
+def test_noise_hkdf_two_output_variant():
+    o = noise.noise_hkdf(b"\x00" * 32, b"ikm", 2)
+    assert len(o) == 2 and all(len(x) == 32 for x in o)
